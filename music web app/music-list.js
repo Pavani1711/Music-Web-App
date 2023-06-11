92c8e244e6475fd0467700e3e162f11f9d616b68
Pavani1711 <133211000+Pavani1711@users.noreply.github.com>
2023-06-11 18:49:53 +0530

    Add files via upload

---
+++ music web app/music-list.js
@@ -0,0 +1,88 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "1. Chilipiga",
+    artist: "Karthik",
+    img: "song5",
+    src: "song5",
+  },
+  {
+    name: "2. Chiru Chiru Chiru",
+    artist: "Haricharan, Tanvi",
+    img: "song6",
+    src: "song6",
+  },
+  {
+    name: "3. Mastaaru Mastaaru",
+    artist: "Shwetha Mohan",
+    img: "song3",
+    src: "song3",
+  },
+  {
+    name: "4. Megham Karigena",
+    artist: "Anudeep, Krishna Kanth",
+    img: "song9",
+    src: "song9",
+  },
+  {
+    name: "5. Naa Madhi",
+    artist: "Dhanunjay, Srinivas Mouli",
+    img: "song2",
+    src: "song2",
+  },
+  {
+    name: "6. Nee Yadalo Naaku",
+    artist: "Yuvan Shankar Raja, Tanvi",
+    img: "song7",
+    src: "song7",
+  },
+  {
+    name: "7. Pilla Padesave",
+    artist: "Haricharan",
+    img: "song10",
+    src: "song10",
+  },
+  {
+    name: "8. Pranam Pothunna",
+    artist: "Yuvan Shankar Raja",
+    img: "song4",
+    src: "song4",
+  },
+  {
+    name: "9. Rooba Rooba",
+    artist: "Sahil Chada, Chinmayi",
+    img: "song8",
+    src: "song8",
+  },
+  {
+    name: "10. Urike Urike",
+    artist: "Sid Sriram, Ramya Behera",
+    img : "song1",
+    src: "song1",
+  },
+   
+    
+    
+    
+    
+    
+    
+    
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
